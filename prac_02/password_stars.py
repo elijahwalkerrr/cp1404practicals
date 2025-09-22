d15da0c73629711a2eff23d99c8bb21942ecb4eb
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-09-22 17:28:53 +1000

    Add password check program.

---
+++ prac_02/password_stars.py
@@ -0,0 +1,6 @@
+password = input("New Password: ")
+
+while len(password) < 8:
+    print ("Invalid Password Length")
+    password = input("New Password: ")
+print ("Password Set: " + "*" * len(password))
