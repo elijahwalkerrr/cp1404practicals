4ce9d7950b2b589a9b3f6928a800ed5869f88b59
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-09-22 17:58:46 +1000

    Add random score and function

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,31 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+
+def main ():
+    score = float(input("Enter score: "))
+    status = score_status(score)
+    print(status)
+    random_score = random.randint(0,100)
+    random_status = score_status(random_score)
+    print(f"A random score is {random_score}, that is {random_status}!")
+
+
+def score_status (score):
+    if score < 0 or score > 100:
+        status = "Invalid score"
+        return status
+    elif score >= 90:
+        status = "Excellent"
+        return status
+    elif score >= 50:
+        status = "Passable"
+        return status
+    else:
+        status ="Bad"
+        return status
+
+main()
