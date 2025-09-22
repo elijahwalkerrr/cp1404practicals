3ea610388554e469a1bc55917d9e59e259bbc1a2
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-09-22 17:36:17 +1000

    Add password check program.

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,20 @@
 
 
 def main():
+    get_password()
+
+
+def get_password():
     password = input("New Password: ")
     while len(password) < 8:
-        print ("Invalid Password Length")
+        print("Invalid Password Length")
         password = input("New Password: ")
-    print ("Password Set: " + "*" * len(password))
+    print_password(password)
+
+
+def print_password(password: str):
+    print("Password Set: " + "*" * len(password))
+
 
 main()
+
