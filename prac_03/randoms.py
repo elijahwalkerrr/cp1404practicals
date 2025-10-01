8418d0f4959b04a8e1e5f56a85b7a0f713d6a3fc
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-10-01 16:12:50 +1000

    Add randoms

---
+++ prac_03/randoms.py
@@ -0,0 +1,7 @@
+# 5 and 20
+# 3 and 9, line two could not have produced a two due to the 2 step, skipping even numbers.
+# 2.5
+
+import random
+random_number = random.randint(1,100)
+print(random_number)
