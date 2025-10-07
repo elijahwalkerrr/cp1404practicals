386136ca9abd644bb77640f69f24be77ba379db0
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-10-08 09:22:05 +1000

    Add total income

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -3,22 +3,27 @@ CP1404/CP5632 Practical
 Starter code for cumulative total income program
 """
 
+
 def main():
-    """Display income report for incomes over a given number of months."""
+    """Display income report for incomes over a given number of num_months."""
     incomes = []
-    months = int(input("How many months? "))
-    print_income(incomes, months)
+    num_months = int(input("How many num_months? "))
+    collect_income(incomes, num_months)
+    print_income(incomes, num_months)
+
+def print_income(incomes: list[any], num_months: int):
     print("\nIncome Report\n-------------")
     total = 0
-    for number_of_months in range(1, months + 1):
-        income = incomes[number_of_months - 1]
+    for month in range(1, num_months + 1):
+        income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(number_of_months, income, total))
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
 
-def print_income(incomes: list[any], months: int):
-    for number_of_months in range(1, months + 1):
-        income = float(input(f"Enter income for number_of_months {str(number_of_months)}: "))
+def collect_income(incomes: list[any], num_months: int):
+    for month in range(1, num_months + 1):
+        income = float(input("Enter income for month " + str(month) + ": "))
         incomes.append(income)
+
 
 main()
