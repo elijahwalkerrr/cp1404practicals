fb2d59df5cef05d9621a6bce37b00df3e891fd1b
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-10-14 11:49:12 +1000

    Add email file

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -14,17 +14,22 @@ def main():
     print(email_names)
     email = input("Enter Your Email: ")
     name = get_name_from_email(email)
+ for email,name in email_names.items():
+     print(f"{name}, {email}")
 
 
 
 def get_name_from_email(email):
-    """Split the name from the email and check if it's correct """
-    email_parts = email.split("@")
-    check = input(f"Is {email_parts[0]}  your name? (T/F)")
-    if check == "T":
+    if email != "":
+     """Split the name from the email and check if it's correct """
+     email_parts = email.split("@")
+     check = input(f"Is {email_parts[0]}  your name? (T/F)")
+     if check == "T":
         name = email_parts[0]
-    else:
+     else:
         name = input("Please Enter Name: ")
-    return name
+     return name
+    else:
+        return ""
 
 main()
