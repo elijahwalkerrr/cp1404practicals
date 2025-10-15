22f966382e3350e4f3aba9bf21ee0dae0007026c
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-10-15 15:17:47 +1000

    Finish Wimbledon

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,19 +1,45 @@
-FILENAME = "wimbledon.csv"
+def line_extraction(line, champion_countries):
+    file_parts = line.split(",")
+    champions = file_parts[2]
+    countries = file_parts[3]
+    champion_countries[countries] = champions
+    return champions, champion_countries
 
 
-with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+def count_champions(champion_count, champions):
+    if champions in champion_count:
+        champion_count[champions] += 1
+    else:
+        champion_count[champions] = 1
+    return champion_count
+
+
+def print_countries_and_champions(champion_count, champion_countries):
+    for champions, count in champion_count.items():
+        print(f"{champions}, {count}")
+    winning_countries = list(champion_countries.keys())
+    print(f"\nThese {len(winning_countries)} Countries have won Wimbledon:")
+    print(", ".join(winning_countries))
+
+
+def main():
+    filename = "wimbledon.csv"
     champion_count = {}
-    champion_countries ={}
-    for line in in_file:
-        file_parts = line.split(",")
-        champions = file_parts[2]
-        countries = file_parts[3]
-        champion_countries[champions] = countries
-        if champions in champion_count:
-                champion_count[champions] += 1
-        else:
-                champion_count[champions] = 1
-    print(f"{champions},{champion_count}")
-    print(champion_countries.items())
+    champion_countries = {}
+    file_number = 0
 
-in_file.close()
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        for line in in_file:
+            file_number += 1
+            if file_number == 1:
+                continue
+            champions, champion_countries = line_extraction(line, champion_countries)
+            champion_count = count_champions(champion_count, champions)
+
+    print_countries_and_champions(champion_count, champion_countries)
+
+
+main()
+
+
+
