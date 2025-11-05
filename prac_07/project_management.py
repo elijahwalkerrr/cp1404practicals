cc6d0ce622c55466391bfd9ebe945808352385ca
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-11-05 12:01:40 +1000

    Add project addition function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,10 +1,30 @@
 """ Time Estimation: 40mins """
 from prac_07.project import Project
 
+FILENAME = "projects.txt"
 
 def main():
-    projects =file_load("projects.txt")
-    display_projects(projects)
+    option = print_menu().lower()
+    projects = []
+    while option != "q":
+        if option == "l":
+            projects = file_load(FILENAME)
+            option = print_menu().lower()
+            if not projects:
+                projects = file_load(FILENAME)
+            elif option == "s":
+                save_file(FILENAME)
+                option = print_menu().lower()
+            elif option == "f":
+                projects.sort
+                display_projects(projects)
+                option = print_menu().lower()
+            elif option == "a":
+                projects.append(add_new_project())
+                option = print_menu().lower()
+            elif option == "d":
+                display_projects(projects)
+                option = print_menu().lower()
 
 def print_menu():
     print("(L)oad Projects: "
@@ -12,7 +32,8 @@ def print_menu():
           "(D)isplay Projects: "
           "(F)ilter Projects by Date: "
           "(A)dd New Project: "
-          "(U)pdate Project: ")
+          "(U)pdate Project: "
+          "(Q)uit: ")
     option = input("Select From Menu: ")
     return option
 
@@ -37,6 +58,18 @@ def save_file(filename, projects):
         for project in projects:
             print(f"{project.name}\t{project.start_date}\t{project.priority}\t{project.cost_est}\t"
                   f"{project.completion}\t")
+
+def add_new_project():
+    name = input("Name: ")
+    start_date = input("Start Date: ")
+    priority = int(input("Priority: "))
+    cost_est = float(input("Cost Estimate: "))
+    completion = float(input("Completion (%): "))
+    return Project(name, start_date, priority,cost_est, completion)
+
+
+
+
 
 
 main()
