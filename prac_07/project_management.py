baeaf2d8e4a674aa1baf3dde6550d51fe54fb6b8
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-11-06 15:37:24 +1000

    Add display status function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -5,41 +5,42 @@ from prac_07.project import Project
 DEFAULT_FILE = "projects.txt"
 
 def main():
+    """Loads the default file"""
     projects = file_load(DEFAULT_FILE)
-    print("")
+    print(f"Default file: {DEFAULT_FILE}\n"
+          f"Currently loaded: {len(projects)} Projects ")
     choice = print_menu().lower()
     while choice != "q":
         if choice == "l":
             filename = input("Please Enter Filename To Load: ")
             projects = file_load(filename)
-            display_projects(projects)
+            display_projects_list(projects)
         elif choice == "s":
             filename = input("Please Enter Filename To Save: ")
             save_file(filename, projects)
         elif choice == "f":
             projects.sort
-            display_projects(projects)
+            display_projects_list(projects)
         elif choice == "a":
             projects.append(add_new_project())
-
         elif choice == "d":
-            display_projects(projects)
-
+            display_projects_status(projects, "completed")
+            display_projects_status("incomplete")
         elif choice == "u":
-            display_projects(projects)
+            display_projects_list(projects)
             projects = update_project(projects)
         else:
             print("Invalid Input: Try Again")
         choice = print_menu().lower()
 
 def print_menu():
-    print("\n(L)oad Projects\n"
-          "(S)ave Projects\n"
-          "(D)isplay Projects\n"
-          "(F)ilter Projects by Date\n"
-          "(A)dd New Project\n"
-          "(U)pdate Project\n"
-          "(Q)uit\n")
+    print("\n- (L)oad Projects\n"
+          "- (S)ave Projects\n"
+          "- (D)isplay Projects\n"
+          "- (F)ilter Projects by Date\n"
+          "- (A)dd New Project\n"
+          "- (U)pdate Project\n"
+          "- (Q)uit\n")
     choice = input("Select From Menu: ")
     return choice
 
@@ -53,10 +54,23 @@ def file_load(filename):
             projects.append(project)
     return projects
 
-def display_projects(projects):
+def display_projects_list(projects):
     for i, project in enumerate(projects):
         print(f"{i+1}. {project.name}, start: {project.start_date} priority {project.priority}"
-              f", estimate: ${project.cost_est}, completion: {project.completion}")
+              f", estimate: ${project.cost_est}, completion: {project.completion}%")
+def display_projects_status(projects, status):
+    if {status} == "Completed":
+        print(f"Projects Completed: ")
+        for i, project in enumerate(projects):
+            if project.completed():
+                print(f"{i + 1}. {project.name}, start: {project.start_date} priority {project.priority}"
+              f", estimate: ${project.cost_est}, completion: {project.completion}%")
+    else:
+        print("Projects Incomplete:")
+        for i, project in enumerate(projects):
+            if not project.completed():
+                print(f"{i + 1}. {project.name}, start: {project.start_date} priority {project.priority}"
+                  f", estimate: ${project.cost_est}, completion: {project.completion}%")
 
 def save_file(filename, projects):
     """Saves file with headings included"""
