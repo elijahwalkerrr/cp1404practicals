c40ffafd50d7508a22790d5bd3ca850becbb26c2
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-11-11 13:19:42 +1000

    Add handle greet function.

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -5,8 +5,14 @@ from kivy.lang import Builder
 class BoxLayoutDemo(App):
     def build(self):
         self.title = "Box Layout Demo"
-        self.root = Builder.load_file('8Pbox_layout.kv')
+        self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def handle_greet(self):
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+
+
 
 
 BoxLayoutDemo().run()
