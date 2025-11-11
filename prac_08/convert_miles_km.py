746db99281552f24a8c7c904e60f9560a6460a4e
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-11-11 15:07:35 +1000

    Add conversion of miles to km method.

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,19 +1,30 @@
+"""
+CP1404 Practical
+Kivy GUI program to convert miles to kilometres
+Elijah Walker
+Started 11/11/2025
+"""
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
 
-
-
-
-class ConvertMilesToKilometers(App):
-    """ ConvertMilesToKilometers is a Kivy App for changing miles to kilometers """
+class ConvertMilesToKilometres(App):
+    """ ConvertMilesToKilometres is a Kivy App for changing miles to kilometres """
     def build(self):
+        """ build the Kivy app from the kv file """
+        Window.size = (800, 400)
+        self.title = "Convert Miles To Kilometres"
+        self.root = Builder.load_file('convert_miles_km.py')
+        return self.root
+
+    def handle_conversions(self, mile):
+        """ handle conversion of miles to kilometres, output kilometre to label widget """
+        try:
+            kilometre = float(mile) * 1.609344
+            self.root.ids.output_label.text = str(kilometre)
+        except ValueError:
+            pass
 
 
-
-
-
-
-
-ConvertMilesToKilometers().run()
+ConvertMilesToKilometres().run()
