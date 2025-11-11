c242a42fa13c4bd6e64f7707a15ddf8750c40fb2
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-11-11 14:42:06 +1000

    Add initial class name and build.

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+
+
+
+class ConvertMilesToKilometers(App):
+    """ ConvertMilesToKilometers is a Kivy App for changing miles to kilometers """
+    def build(self):
+
+
+
+
+
+
+
+ConvertMilesToKilometers().run()
