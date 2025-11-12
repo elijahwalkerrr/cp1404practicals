d92b48ef42bcf59f6b2ced99791790345ddade38
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-11-12 10:20:56 +1000

    Add build method for kivy GUI.

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -10,12 +10,20 @@ from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.properties import StringProperty
 
-class DyanmicLabelsApp(App):
+class DynamicLabelsApp(App):
     """"""
     status_text = StringProperty()
 
     def __init__(self, **kwargs):
         """ Construct main app."""
         super().__init__(**kwargs)
-        self.names = ["Hailey", "Phil", "Claire", "Cameron"]
+        self.names = ["Hailey", "Phil", "Claire", "Cameron", "Mitch", "Jay"]
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_widgets()
+        return self.root
+
 
