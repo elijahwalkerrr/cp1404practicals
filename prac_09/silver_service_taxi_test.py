82cca7bf680a4f329c26643f17c520f47859ac76
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-11-17 15:11:36 +1000

    Change test to 48.8 to check if rounds up.

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -7,6 +7,6 @@ def main():
     cool_taxi = SilverServiceTaxi("My Cool Taxi", 250, 2)
     cool_taxi.drive(18)
     fare = cool_taxi.get_fare()
-    expected = 48.78
+    expected = 48.8
     assert round(fare, 2) == expected, f"Fare {fare:.2f} != {expected}"
 main()
