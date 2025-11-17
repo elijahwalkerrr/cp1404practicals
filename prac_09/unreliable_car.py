cfb38f4bba33830675bb49089d87cde9f181177a
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-11-17 13:48:55 +1000

    Add alterations to car method.

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,33 @@
+"""
+Derived Class that inherits from Car.
+"""
+import random
+
+from prac_09.car import Car
+
+class UnreliableCar(Car):
+    """Specialised version of a car that includes reliability attribute"""
+    def __init__(self, name="", fuel=0, reliability = 0.0):
+        """Initialise an unreliable Car instance, based on parent class Car."""
+        super.__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        if random.randint(1,100) < self.reliability:
+            if distance > self.fuel:
+                distance = self.fuel
+                self.fuel = 0
+            else:
+                self.fuel -= distance
+            self.odometer += distance
+        else:
+            distance = 0
+        return distance
+
+
+
+
+
+
+
+
