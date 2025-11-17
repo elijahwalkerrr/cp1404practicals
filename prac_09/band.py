40c0e1a7e04c8bf0a0759ff8f4415efbbd8b377d
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-11-17 15:23:32 +1000

    Add docstrings to methods.

--- prac_09/band.py
+++ prac_09/band.py
@@ -3,17 +3,21 @@ Band Class for Practical 9
 """
 
 class Band:
+    """A band consisting of multiple musicians."""
     def __init__(self, name):
         self.name = name
         self.musicians = []
 
     def add(self, musician):
+        """ Adds a musician to the band list. """
         self.musicians.append(musician)
 
     def __str__(self):
+        """Prints the name of each member of the band"""
         return f"{self.name} ({', '.join(str(musicians) for musicians in self.musicians)})"
 
     def play(self):
+        """Iterates through each musician and checks for instrument."""
         for musician in self.musicians:
             instrument = musician.get_instrument()
             if instrument:
