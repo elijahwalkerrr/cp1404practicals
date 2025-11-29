fa81719ef62df43b5f545810240fdd715c4a1174
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-11-29 16:35:17 +1000

    Add celsius conversion to flask app.

--- prac_10/app.py
+++ prac_10/app.py
@@ -10,3 +10,21 @@ def hello():
 @app.route('/greet/<name>')
 def greet(name=""):
     return f"Hello {name}"
+
+
+def c_to_f(celsius):
+    return celsius * 9 / 5 + 32
+
+
+@app.route('/convert/<celsius_value>')
+def convert(celsius_value):
+    try:
+        celsius = float(celsius_value)
+        f = c_to_f(celsius)
+        return f"{celsius}°C = {f:.2f}°F"
+    except ValueError:
+        return "Invalid number. Try: /convert/25"
+
+
+if __name__ == "__main__":
+    app.run()
