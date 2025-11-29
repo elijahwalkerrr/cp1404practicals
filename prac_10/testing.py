c102884aefd5fa6b6ecbe2e401626d9018aedbe1
elijahwalkerrr <elijah.walker@my.jcu.edu.au>
2025-11-29 17:05:33 +1000

    Remove TO DO throughout final.

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -48,15 +48,9 @@ def run_tests():
     # the test below should fail (now works)
     assert repeat_string("hi", 2) == "hi hi"
 
-    # assert test with custom message,
-    # used to see if Car's init method sets the odometer correctly
-    # this should pass (no output)
     car = Car()
     assert car._odometer == 0, "Car does not set odometer correctly"
 
-    # Note that Car's __init__ function sets the fuel in one of two ways:
-    # using the value passed in or the default
-    # You should test both of these
     assert car.fuel == 0, "Default fuel does not set correctly"
 
     car_with_fuel = Car(fuel=10)
@@ -66,13 +60,3 @@ run_tests()
 
 # Enable the docsets
 doctest.testmod()
-
-# TODO: 5. Write and test a function to format a phrase as a sentence,
-# starting with a capital and ending with a single full stop.
-# Important: start with a function header and just use pass as the body
-# then add doctests for 3 tests:
-#   'hello' -> 'Hello.'
-#   'It is an ex parrot.' -> 'It is an ex parrot.'
-# and one more that you decide is a useful test.
-# Run your doctests and watch the tests fail.
-# Then write the body of the function so that the tests pass.
